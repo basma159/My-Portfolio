2f7d89c0fd8c6f2e57256d8ac63b4a152db086f9
Basma <bsmtbdalawl@gmail.com>
2026-08-03 02:23:49 +0300

    Fix Tailwind build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/My-Portfolio/'
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/My-Portfolio/',
 })
